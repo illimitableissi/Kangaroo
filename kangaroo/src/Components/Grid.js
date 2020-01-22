43d68f4784051bf7c1e0a5770530e0404ddc6e05
SOVEREIGN <isimemeni@gmail.com>
2020-01-21 19:00:08 -0500

    added components

---
+++ kangaroo/src/Components/Grid.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+export const Container = ({children}) => {
+    return (
+        <div className="row">
+            {children}
+        </div>
+    );
+};
+
+export const Row = ({children}) => {
+    return (
+        <div className="row">
+            {children}
+        </div>
+    );
+};
+
+export const Column = ({children}) => {
+    return (
+        <div className="col">
+            {children}
+        </div>
+    );
+};
