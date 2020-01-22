34534307217df7b01355291c3a994df6e1e08dbd
SOVEREIGN <isimemeni@gmail.com>
2020-01-21 19:52:58 -0500

    working on nav bar

--- kangaroo/src/Components/Nav.js
+++ kangaroo/src/Components/Nav.js
@@ -2,9 +2,14 @@ import React from 'react';
 
 const Nav = () => {
     return (
-        <div className="Nav">
-            
-        </div>
+        <div>
+        <nav className="navbar navbar-light bg-light">
+        <a className="navbar-brand" href="#">
+          <img src="images/icon.PNG" width="30" height="30" className="d-inline-block align-top" alt="">
+          Kangaroo</img>
+        </a>
+      </nav>
+      </div>
     );
 };
 
