87fbdf222a27005f2cc4d8120a6baf9cb8d87602
SOVEREIGN <isimemeni@gmail.com>
2020-01-25 10:32:37 -0500

    added Pages directory

---
+++ kangaroo/src/Pages/HomePage.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Jumbotron from './Components/Jumbotron';
+import Parallax from './Components/Parallax';
+import {Container, Row, Column} from './Components/Grid';
+import Card from './Components/Card';
+import Images from './Components/Images'
+import Nav from './Components/Nav'
+
+function HomePage() {
+  return (
+    <div className="App">
+      <Parallax>
+      <Jumbotron />
+        <Container>
+          <br />
+          <Card img="/images/lease.PNG" aos="fade-right"/>
+          <Card img="/images/packing.PNG" aos="fade-left"/>
+          <Card img="/images/box.PNG" aos="fade-right"/>
+        </ Container>
+      </ Parallax>
+      
+
+    </div>
+  );
+}
+
+export default HomePage;
