6594ca169b4cb734f495b5c51240fd46bf5ff94e
SOVEREIGN <isimemeni@gmail.com>
2020-02-07 00:43:02 -0500

    Added carousel to listing detail page

--- kangaroo/src/Pages/ListingDetail.js
+++ kangaroo/src/Pages/ListingDetail.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Nav from '../Components/Nav'
 import Parallax from "../Components/Parallax";
-import API from '../utils/API'
+import API from '../utils/API';
+import Carousel from 'react-bootstrap/Carousel'
 
 class ListingDetail extends React.Component {
     state = {
@@ -17,11 +18,39 @@ class ListingDetail extends React.Component {
       render () {
           return (
             <div>
-                <Parallax>
                 <Nav />
+                <Carousel>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="/images/apartment-complex.jpg"
+                        alt="First slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="/images/interior.jpg"
+                        alt="Third slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="/images/pool.jpg"
+                        alt="Third slide"
+                        />
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <img
+                        className="d-block w-100"
+                        src="/images/insideApartment.jpg"
+                        alt="Third slide"
+                        />
+                     </Carousel.Item>
+                </Carousel>                 
                 <h1>{this.state.listing.location}</h1>
                 <a href="/search" role="button" className="btn btn-danger">Go Back</a>
-                </Parallax>
             </div>  
         );
       }
