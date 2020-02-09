46c2eec60729ecfe449a2d54c7240b0b476bed4d
SOVEREIGN <isimemeni@gmail.com>
2020-02-09 15:27:07 -0500

    chaged server info

--- server.js
+++ server.js
@@ -36,5 +36,5 @@ require("./config/passport")(passport);
 app.use(routes);
 app.use("/api/users", users);
 
-const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+const port = process.env.MONGODB_URI || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
