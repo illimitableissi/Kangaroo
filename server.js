04cef2bb97ae5bc6bf51b01b2c17920fad4e5c6b
Colin Green <colin.green@knights.ucf.edu>
2020-02-09 17:20:33 -0500

    Trying to deploy to heroku (Colin)

--- server.js
+++ server.js
@@ -1,50 +1,56 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const passport = require("passport");
-const users = require("./routes/api/users");
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const users = require('./routes/api/users');
 const app = express();
-const routes = require("./routes");
+const routes = require('./routes');
+const path = require('path');
 
 // Bodyparser middleware
 app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
+	bodyParser.urlencoded({
+		extended: true
+	})
 );
 app.use(bodyParser.json());
 
 // DB Config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 
 // Connect to MongoDB
 mongoose
-  .connect(
-    process.env.MONGODB_URI ||
-    db,
-    { useNewUrlParser: true }
-  )
-  .then(() => console.log("MongoDB successfully connected"))
-  .catch(err => console.log(err));
+	.connect(process.env.MONGODB_URI || db, { useNewUrlParser: true })
+	.then(() => console.log('MongoDB successfully connected'))
+	.catch(err => console.log(err));
 
-  // Passport middleware
+// Passport middleware
 app.use(passport.initialize());
 
 // Passport config
-require("./config/passport")(passport);
+require('./config/passport')(passport);
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
+// if (process.env.NODE_ENV === 'production') {
+// 	app.use(express.static('client/build'));
+// }
 
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+app.get('*', function(req, res) {
+	res.sendFile(path.join(__dirname, './client/build/index.html'));
 });
-
 
 // Routes
 app.use(routes);
-app.use("/api/users", users);
+app.use('/api/users', users);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === 'production') {
+	// Set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+	});
+}
 
 const port = process.env.MONGODB_URI || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
