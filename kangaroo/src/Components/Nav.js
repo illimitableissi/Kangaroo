125b682b055cc9804ac4f0a422eb76936a0bf95f
Colin Green <colin.green@knights.ucf.edu>
2020-02-11 12:28:33 -0500

    Updated the NavBar, still trying to get API links working

--- kangaroo/src/Components/Nav.js
+++ kangaroo/src/Components/Nav.js
@@ -1,14 +1,31 @@
 import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
-const Nav = () => {
-    return (
-        <div>
-        <nav className="navbar navbar-light bg-light">
-        <a className="navbar-brand" href="/">Kangaroo</a>
-          <img src="/images/icon.PNG" width="30" height="30" className="d-inline-block align-top" alt=""></img>  
-      </nav>
-      </div>
-    );
-};
+class MyNavbar extends React.Component {
+	render() {
+		return (
+			<Navbar bg='light' expand='lg'>
+				<Navbar.Brand href='/'>
+					<img
+						src='/images/icon.PNG'
+						width='30'
+						height='30'
+						className='d-inline-block align-top'
+						alt='React Bootstrap logo'
+					/>
+					Kangaroo
+				</Navbar.Brand>
+				<Navbar.Toggle aria-controls='basic-navbar-nav' />
+				<Navbar.Collapse id='basic-navbar-nav'>
+					<Nav className='mr-auto'>
+						<Nav.Link href='/api/users'>Users API</Nav.Link>
+						<Nav.Link href='/api/listings'>Listings API</Nav.Link>
+					</Nav>
+				</Navbar.Collapse>
+			</Navbar>
+		);
+	}
+}
 
-export default Nav;
+export default MyNavbar;
--- routes/api/listing.js
+++ routes/api/listing.js
@@ -1,23 +1,22 @@
-const router = require("express").Router();
-const listingController = require("../../controllers/listingController")
+const router = require('express').Router();
+const listingController = require('../../controllers/listingController');
 
-router.route("/")
-    .get(listingController.findAll)
-    .post(listingController.create);
+router
+	.route('/')
+	.get(listingController.findAll)
+	.post(listingController.create);
 
-router.route("/location")
-    .get(listingController.findByLocation);
+router
+	.route('/location/:id/submit')
+	.get(listingController.findById)
+	.post(listingController.update);
 
-router.route("/location/:id/submit")
-    .get(listingController.findById)
-    .post(listingController.update);
+router
+	.route('/:id')
+	.get(listingController.findById)
+	.post(listingController.update)
+	.delete(listingController.remove);
 
-router.route("/:id")
-    .get(listingController.findById)
-    .post(listingController.update)
-    .delete(listingController.remove)
-
-router.route("/userName")
-    .get(listingController.findByUserName)
+router.route('/userName').get(listingController.findByUserName);
 
 module.exports = router;
